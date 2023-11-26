ffd808b9e202ec483179e54007b491c19993a15b
Shamsur-Rahaman <shamsurrahaman132@gmail.com>
2023-11-26 12:40:45 +0600

    small project complete

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
 
-//parser
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+//parser
 
-console.log(process.cwd());
+const getAController = (req: Request, res: Response) => {
+  const a: number = 10;
+  res.send('Hello World! ' + a);
+};
+app.get('/', getAController);
+
+// console.log(process.cwd());
 
 export default app;
---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,37 @@
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNumber: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNumber: string;
+};
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth?: string;
+  contactNumber: string;
+  emergencyContact: string;
+  bloodGroup?: 'A+' | 'A-' | 'AB+' | 'AB-' | 'O+' | 'O-' | 'B+' | 'B-';
+  email: string;
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+};
+
+// export Student;
